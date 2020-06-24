2f25d61cee1b9153d73bfc082a1f37b1d48434f8
Srini Venkat <srini6teen@gmail.com>
2020-06-24 14:44:56 -0700

    Second commit

--- src/main/java/com/test/FirstClass.java
+++ src/main/java/com/test/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("This is a print statement");
+        System.out.println("This is a second print statement");
     }
 }
